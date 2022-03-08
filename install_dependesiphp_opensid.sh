fe9505bf7286165d9b1441f6acb759829a2c6f19
jeffrynasri <jeffryfaruki@gmail.com>
2022-03-08 09:33:13 +0700

    Create install_dependesiphp_opensid.sh

---
+++ install_dependesiphp_opensid.sh
@@ -0,0 +1,11 @@
+#Menggunakan php 7.4
+apt-get install -y php7.4-curl
+apt-get install -y php7.4-fileinfo
+apt-get install -y php7.4-gd
+apt-get install -y php7.4-iconv
+apt-get install -y php7.4-json
+apt-get install -y php7.4-mbstring
+apt-get install -y php7.4-mysqli
+apt-get install -y php7.4-mysqlnd
+apt-get install -y php7.4-tidy
+apt-get install -y php7.4-zip
